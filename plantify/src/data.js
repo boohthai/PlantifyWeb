8d8b6cb53365b7ac17d5671566c4cf604693cd46
boohthai <thaithanhtai999@gmail.com>
2021-11-01 21:29:41 +0700

    UI

---
+++ plantify/src/data.js
@@ -0,0 +1,79 @@
+import tree1 from '../../youtube-react-shop-ui/src/assets/tree1.png'
+import tree2 from '../../youtube-react-shop-ui/src/assets/tree2.png'
+import tree3 from '../../youtube-react-shop-ui/src/assets/tree3.png'
+export const sliderItems = [
+    {
+      id: 1,
+      img: tree1,
+      title: "PEPEROMIA",
+      desc: "Let's fresh our workspace",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: tree2,
+      title: "BIRD'S NEST FERN",
+      desc: "AIR PURIFIER",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: tree3,
+      title: "CROTON PETRA",
+      desc: "No green thumb required to keep our artificial watermelon peperomia plant looking lively and lush anywhere you place it.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://science4fun.info/wp-content/uploads/2017/05/flower-of-a-plant.jpg",
+      title: "Flowering plants",
+    },
+    {
+      id: 2,
+      img: "https://cdn.britannica.com/s:800x450,c:crop/30/73130-138-917E422E/Ferns-systems-tracheophytes-leaves-water.jpg",
+      title: "Ferns",
+    },
+    {
+      id: 3,
+      img: "https://i.guim.co.uk/img/media/a3a1b1b1d2588568f6463a1db86f2507cbd3d420/0_146_4272_2563/master/4272.jpg?width=445&quality=45&auto=format&fit=max&dpr=2&s=714e8795bf434a7d1d5d86750beea87a",
+      title: "Climbers",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.costafarms.com/images/SlideShow/Croton-Popular-Houseplants-Cota-Farms.jpg",
+    },
+    {
+      id:2,
+      img:"https://www.costafarms.com/images/SlideShow/Dracaena-Popular-Houseplants-Costa-Farms.jpg",
+    },
+    {
+      id:3,
+      img:"https://www.costafarms.com/images/SlideShow/Phalaenopsis-Orchid-Romantic-Plant-Costa-Farms.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.costafarms.com/images/SlideShow/Anthurium-Flowering-Houseplant-Costa-Farms.jpg",
+    },
+    {
+      id:5,
+      img:"https://www.costafarms.com/images/SlideShow/Pothos-Houseplant-Costa-Farms.jpg",
+    },
+    {
+      id:6,
+      img:"https://www.costafarms.com/images/SlideShow/1-Costa-Farms-Lucky-Bamboo-Houseplant.jpg",
+    },
+    {
+      id:7,
+      img:"https://www.costafarms.com/images/SlideShow/Dracaena-Marginata-Houseplant-Costa-Farms.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.costafarms.com/images/SlideShow/1-Costa-Farms-Sansevieria-Snake-Plant-Houseplant.jpg",
+    },
+  ]
